7ec6b12f44bd0e81bf4afd579ef7d4f9577705a7
Yasath Amasara <yasathamasara2@gmail.com>
2025-06-18 19:50:15 +0530

    Update loging form

--- loging form interface.cpp
+++ loging form interface.cpp
@@ -4,14 +4,41 @@ using namespace std;
 
 int main(){
 
+  class temp{
+     string username,email,password;
+     fstream fil;
+      public:
+      void login();
+      void signUP();
+      void forgot();
+}obj;
+
   char choice;
   cout<<"\n1-login";
   cout<<"\n2-sign up";
   cout<<"\n3-forgot password";
   cout<<"\n4-exit";
   cout<<"\nEnter your choice: ";
-  cin>>choice
-  
+  cin>>choice;
+
+  switch(choice){
+        case'1':
+
+    break;
+        case'2':
+
+    break;
+        case'3':
+
+    break;
+        case'4':
+
+    break;
+    defualt:
+      cout<<"inalid selection.....!";
+    
+
+  }
 
 
 }
