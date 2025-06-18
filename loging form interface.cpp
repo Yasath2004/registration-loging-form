49e520c0e3bd2b9506f8fd517ee08f10a2fe8ef6
Yasath Amasara <yasathamasara2@gmail.com>
2025-06-18 19:29:53 +0530

    Update  loging form interface.cpp
    base interface in login form

---
+++ loging form interface.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+
+  char choice;
+  cout<<"\n1-login";
+  cout<<"\n2-sign up";
+  cout<<"\n3-forgot password";
+  cout<<"\n4-exit";
+  cout<<"\nEnter your choice: ";
+  cin>>choice
+  
+
+
+}
